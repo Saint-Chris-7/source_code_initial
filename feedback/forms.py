1ec9936e95e427a8be8647164ac82e9045d262d0
Saint-Chris-7 <chrisgathuthi@gmail.com>
2022-10-14 13:56:40 +0300

    django-with celery

---
+++ feedback/forms.py
@@ -0,0 +1,16 @@
+
+
+from feedback.tasks import send_feedback_email_task
+
+from django import forms
+
+class FeedbackForm(forms.Form):
+    email = forms.EmailField(label="Email Address")
+    message = forms.CharField(
+        label="Message", widget=forms.Textarea(attrs={"rows": 5})
+    )
+
+    def send_email(self):
+        send_feedback_email_task.delay(
+            self.cleaned_data["email"], self.cleaned_data["message"]
+        )
