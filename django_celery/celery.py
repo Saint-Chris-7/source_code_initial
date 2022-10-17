e21ec408a640cd77dce21f18ca5892a3e39765b5
Saint-Chris-7 <chrisgathuthi@gmail.com>
2022-10-17 15:59:46 +0300

    celery scheduling

--- django_celery/celery.py
+++ django_celery/celery.py
@@ -6,4 +6,4 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
 app = Celery("django_celery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+app.autodiscover_tasks()#this will read any module with tasks.py in our project
--- feedback/tasks.py
+++ feedback/tasks.py
@@ -1,6 +1,8 @@
 from time import sleep
 from django.core.mail import send_mail
 from celery import shared_task
+import random
+
 
 
 @shared_task
@@ -14,3 +16,23 @@ def send_feedback_email_task(email_address, message):
         [email_address],
         fail_silently=False,
     )
+
+
+
+
+@shared_task
+def add(x, y):
+    # Celery recognizes this as the `app.tasks.add` task
+    # the name is purposefully omitted here.
+    return x + y
+
+@shared_task(name="multiply_two_numbers")
+def mul(x, y):
+    # Celery recognizes this as the `multiple_two_numbers` task
+    total = x * (y * random.randint(3, 100))
+    return total
+
+@shared_task(name="sum_list_numbers")
+def xsum(numbers):
+    # Celery recognizes this as the `sum_list_numbers` task
+    return sum(numbers)
